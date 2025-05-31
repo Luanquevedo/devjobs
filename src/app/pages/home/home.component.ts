22188e61964a60bea4c031fb3d15fa352603e138
Luan Meira Quevedo <luan.quevedo@hotmail.com>
2025-05-31 13:31:01 -0300

    feat(navbar): Criado componente visual da navbar com estrutura base

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import { SearchBarComponent } from '../../components/search-bar/search-bar.component';
 import { CardsComponent } from '../../components/cards/cards.component';
+import { NavComponent } from '../../components/nav/nav.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
   imports: [
     SearchBarComponent,
+    NavComponent,
     CardsComponent,
   ],
   templateUrl: './home.component.html',
