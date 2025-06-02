5578fd5aec968c052063caf86fbf619bd32828b5
Luan Quevedo <luan.quevedo@hotmail.com>
2025-06-02 11:38:46 -0300

    feat: adicionado logica ao componente cards para que seja renderizado o conteudo de vagas

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,12 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { SearchBarComponent } from '../../components/search-bar/search-bar.component';
 import { CardsComponent } from '../../components/cards/cards.component';
 import { NavComponent } from '../../components/nav/nav.component';
+import { Vacancies } from '../../models/vacancies';
+import { VacanciesApiService } from '../../services/vacancies-api.service';
+
 
 @Component({
   selector: 'app-home',
   standalone: true,
   imports: [
+    CommonModule, // necessario para utilizar map
     SearchBarComponent,
     NavComponent,
     CardsComponent,
@@ -15,5 +20,14 @@ import { NavComponent } from '../../components/nav/nav.component';
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
+vacancies: Vacancies[] = [];
+
+constructor(private vacanciesService: VacanciesApiService) {}
+
+ngOnInit(): void {
+  this.vacanciesService.getAllVacancies().subscribe(data => {
+    this.vacancies = data;
+  });
+}
 
 }
